968c88714f8ecfa611981a841f8ad9dfa1a78d38
kiran <kiran@localhost>
2017-05-31 16:36:31 -0400

    Checkout-Starts populating selected product in the checkout product list.

--- InventoryMgmt-Site/src/main/webapp/static/js/angular/CheckoutController.js
+++ InventoryMgmt-Site/src/main/webapp/static/js/angular/CheckoutController.js
@@ -1,24 +1,28 @@
 var erpApp = angular.module('erpApp', [ 'smart-table' ]);
 
 erpApp.controller('CheckoutController', function($scope) {
-	var selectedProduct = {
-		productImage : "Kir",
-		sNo : 1,
-		sellingPrice : 100,
-		quantity : 10,
-		discount : 1,
-		total : 100
-	};
 	$scope.checkoutProductsList = [];
-	$scope.total = 0;
-
+	$scope.checkoutProductsListSubTotal = 0.0;
+	
 	$scope.addItem = function(selectedItem) {
-		$scope.checkoutProductsList.push(selectedProduct);
-		$scope.total = 120;
+		selectedItem.sNo = $scope.checkoutProductsList.length + 1;
+		$scope.checkoutProductsList.push(selectedItem);
+		
+		$scope.calculateTotals();
 
 		$scope.refresh();
 	};
 
+	$scope.calculateTotals = function() {
+		
+		$scope.checkoutProductsListSubTotal = 0.0;
+		
+		for(index = 0; index < $scope.checkoutProductsList.length; index++){
+			$scope.checkoutProductsListSubTotal = 
+				$scope.checkoutProductsListSubTotal + $scope.checkoutProductsList[index].price.sellingPrice;
+         }
+	}
+	
 	$scope.refresh = function() {
 		setInterval(function() {
 			$scope.$apply()
